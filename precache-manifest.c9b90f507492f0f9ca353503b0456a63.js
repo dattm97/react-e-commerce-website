c7a85aeb8aab6bb6aaa831aae1398c8761c0de3a
dattm <dattm@edoctor.vn>
2022-08-22 11:37:09 +0700

    first commit

---
+++ precache-manifest.c9b90f507492f0f9ca353503b0456a63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/static/media/shopping-bag.09e6f5e7.svg"
+  },
+  {
+    "revision": "4290bc807f898ade8bc9a4296735ab10",
+    "url": "/static/media/newLogo.4290bc80.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5feff516cbd1037448b5",
+    "url": "/static/js/main.a9ac1f04.chunk.js"
+  },
+  {
+    "revision": "c44af2f97180d850d7d6",
+    "url": "/static/js/2.bc4e413d.chunk.js"
+  },
+  {
+    "revision": "5feff516cbd1037448b5",
+    "url": "/static/css/main.d35b6dba.chunk.css"
+  },
+  {
+    "revision": "5f37fd644dd9644c50dd8fd3738f2728",
+    "url": "/index.html"
+  }
+];
